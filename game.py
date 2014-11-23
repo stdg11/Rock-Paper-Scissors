daeb9a0c336c5138faf4473207a6f0517c1934fb
Daniel Grammatica <daniel.grammatica@bourne-grammar.lincs.sch.uk>
2014-11-23 14:51:15 +0000

    Added Computer

--- game.py
+++ game.py
@@ -1,8 +1,31 @@
 import random
 
-human = input("Enter your choice (rock/paper/scissors): ");
-human = human.lower();
+player = input("Enter your choice (rock/paper/scissors): ")
+player = player.lower()
 while (player!="rock" and player!="paper" and player!="scissors"):
-    player = input("%s is not valid. Please re-enter your choice (rock/paper/scissors)" % player);
-    player = player.lower();
+    player = input("%s is not valid. Please re-enter your choice (rock/paper/scissors)" % player)
+    player = player.lower()
 
+computer = ['rock','paper','scissors']
+computer = random.choice(computer)
+
+if (player == computer):
+    print("Draw")
+elif (player == "rock"):
+    if (computer =="paper"):
+        print("Computer Wins!")
+    else:
+        print("You Win!")
+elif (player == "paper"):
+    if (computer == "scissors"):
+        print("Computer Wins!")
+    else:
+        print("You Win!")
+elif (player == "scissors"):
+    if (computer == "rock"):
+        print("Computer Wins!")
+    else:
+        print("You Win!")
+
+print("You chose %s \nComputer picked %s \n Thank you for playing!") % (player, computer)
+input("Press any key to exit.")
