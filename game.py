8a4461675158886604370b62c00c001b99eea1a2
Daniel Grammatica <daniel.grammatica@bourne-grammar.lincs.sch.uk>
2014-11-23 17:07:05 +0000

    Added Play again

--- game.py
+++ game.py
@@ -1,31 +1,43 @@
 import random
+playing = True
 
-player = input("Enter your choice (rock/paper/scissors): ")
-player = player.lower()
-while (player!="rock" and player!="paper" and player!="scissors"):
-    player = input("%s is not valid. Please re-enter your choice (rock/paper/scissors)" % player)
-    player = player.lower()
+def computer_choice():
+    computer = ['rock','paper','scissors']
+    computer = random.choice(computer)
+    return computer
 
-computer = ['rock','paper','scissors']
-computer = random.choice(computer)
-
-if (player == computer):
-    print("Draw")
-elif (player == "rock"):
-    if (computer =="paper"):
-        print("Computer Wins!")
+def play_hand(player, computer):
+    if (player == computer):
+        winner = 'draw'
+    elif (player == "rock" and computer == "paper") or (player == "paper" and computer == "scissors") or (player == "scissors" and computer == "rock"):
+        winner = 'computer'
     else:
-        print("You Win!")
-elif (player == "paper"):
-    if (computer == "scissors"):
-        print("Computer Wins!")
-    else:
-        print("You Win!")
-elif (player == "scissors"):
-    if (computer == "rock"):
-        print("Computer Wins!")
-    else:
-        print("You Win!")
+        winner = 'player'
+    return winner
 
-print("You chose: %s\nComputer picked: %s\n Thank you for playing!" % (player, computer))
+def play_game():
+    playing = True
+    while playing:
+        player = input("Enter your choice (rock/paper/scissors): ")
+        player = player.lower()
+        while (player!="rock" and player!="paper" and player!="scissors"):
+            player = input("%s is not valid. Please re-enter your choice (rock/paper/scissors)" % player)
+            player = player.lower()
+        computer = computer_choice()
+        winner = play_hand(player, computer)
+        if (winner == 'computer'):
+            print("\nUnlucky Computer Won")
+        elif (winner == 'player'):
+            print("\nCongratulations you Won!")
+        elif (winner == 'draw'):
+            print("\nIt's a Draw")
+        else:
+            print("\nOops..")
+        print("You chose: %s | Computer picked: %s" % (player, computer))
+        play_again = input("\nPlay again? (Y/N)")
+        if (play_again == 'N' or play_again == 'n'):
+            playing = False
+
+print("Let's play Rock, Paper, Scissors!")
+play_game()
 input("Press any key to exit.")
